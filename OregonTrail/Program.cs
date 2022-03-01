31eb96813d69d82956fe91328214dc5b05c75256
AustinDenofrio <denofrioaustin@gmail.com>
2022-03-01 14:01:16 -0500

    Added introduction statement to game

--- OregonTrail/Program.cs
+++ OregonTrail/Program.cs
@@ -6,7 +6,13 @@ namespace OregonTrail
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Welcome to the Trailblazer's Inn!");
+            Console.WriteLine(" ");
+            Console.WriteLine("A start point for many explorers looking to make a name for themselves.");
+            Console.WriteLine("The journey ahead is full of risks and rewards as you make your journey West!");
+            Console.WriteLine("I hope you enjoy your night here as we load your horse with supplies.");
+            Console.WriteLine(" ");
+            Console.WriteLine("In the morning you will start out on your journey into the new land!");
         }
     }
 }
