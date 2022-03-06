0678eb34f6ae17805695a2dcd27f052a5b3fb7a3
AustinDenofrio <denofrioaustin@gmail.com>
2022-03-05 22:47:04 -0500

    Added Sheriff Snoke at beginning of game to collect playerName. Added some comments for the direction i want to take the game

--- OregonTrail/Program.cs
+++ OregonTrail/Program.cs
@@ -6,6 +6,36 @@ namespace OregonTrail
     {
         static void Main(string[] args)
         {
+
+            //player has to make a name for themself, then guided to inn.
+            Console.WriteLine("Hey there! I dont believe we've met.");
+            Console.WriteLine("I'm Sheriff Snoke. What's your name?");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            string playerName = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Well, nice to meet you " + playerName + ". Since you're new here let me point you in the right direction.");
+            Console.WriteLine("That building right there is the Inn. Head straight there he'll help you with anything you need.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Push any key to go to inn");
+            Console.ReadLine();
+
+
+
+
+            // Inventory (the items carried on person)
+            double money;
+
+            //Store (items for sale)
+
+
+
+            //Wagon (items on buggie)
+
+
+            Console.Clear();
             Console.WriteLine("Hello, Welcome to the Trailblazer's Inn!");
             Console.WriteLine(" ");
             Console.WriteLine("A start point for many explorers looking to make a name for themselves.");
