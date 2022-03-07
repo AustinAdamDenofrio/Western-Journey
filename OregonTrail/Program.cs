fd4cb762752caa711f086346d30cece853a8dfb6
AustinDenofrio <denofrioaustin@gmail.com>
2022-03-06 20:14:46 -0500

    Changed method of writing to a new line from WriteLine space to the proper \n

--- OregonTrail/Program.cs
+++ OregonTrail/Program.cs
@@ -36,12 +36,10 @@ namespace OregonTrail
 
 
             Console.Clear();
-            Console.WriteLine("Hello, Welcome to the Trailblazer's Inn!");
-            Console.WriteLine(" ");
+            Console.WriteLine("Hello, Welcome to the Trailblazer's Inn!\n");
             Console.WriteLine("A start point for many explorers looking to make a name for themselves.");
             Console.WriteLine("The journey ahead is full of risks and rewards as you make your journey West!");
-            Console.WriteLine("I hope you enjoy your night here as we load your horse with supplies.");
-            Console.WriteLine(" ");
+            Console.WriteLine("I hope you enjoy your night here as we load your horse with supplies.\n");
             Console.WriteLine("In the morning you will start out on your journey into the new land!");
         }
     }
