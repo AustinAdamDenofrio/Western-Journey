67cfd685a8fb2985b7aee96717f972dfa3ed01bd
AustinDenofrio <denofrioaustin@gmail.com>
2022-03-21 13:08:56 -0400

    Created the while loop for containing the store items available for purchase

--- OregonTrail/Program.cs
+++ OregonTrail/Program.cs
@@ -4,6 +4,7 @@ namespace OregonTrail
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
@@ -31,7 +32,7 @@ namespace OregonTrail
             Console.Clear();
             Console.WriteLine("Well, nice to meet you " + playerName + ". Since you're new here let me point you in the right direction.");
             Console.WriteLine("That building right there is the Inn. Head straight there Connor willl help you with anything you need.\n");
-            Console.WriteLine("Push any key to go to inn\n");
+            Console.WriteLine("Push ENTER to go to inn\n");
             Console.ReadLine();
 
 
@@ -49,12 +50,55 @@ namespace OregonTrail
 
 
 
+            // Player gets to purchase items before heading on adventure
 
             Console.Clear();
             Console.WriteLine("Howdy " + playerName + "! Welcome to the Journeyman's Mercantile." +
                 " There's a large selection of item. \nChoose what you like.\nEhem...\nFor the right price that is.\n\n");
-            Console.Write("Enter the item you'd like to purchase\n  #1: Food\n\n  #2: Ammo\n\n  #3: Repair kit\n\n\n");
-            string playerResponse = Console.ReadLine();
+            Console.Write("Enter the item you'd like to purchase\n\n  #1: Food\n\n  #2: Ammo\n\n  #3: Repair kit\n\n\n");
+            
+
+
+            //create a loop that allows the user to buy more than one item.
+
+            bool doneViewingStoreItems = false;
+
+            while (!doneViewingStoreItems) {
+                string playerResponse = Console.ReadLine();
+                if (playerResponse == "1")
+                {
+                    Console.WriteLine("You have entered the number 1");
+                }
+                else if (playerResponse == "2")
+                {
+                    Console.WriteLine("You have chosen 2");
+                }
+                else if (playerResponse == "3")
+                {
+                    Console.WriteLine("You have chosen 3");
+                }
+                else if (playerResponse == "4")
+                {
+                    Console.WriteLine("You have chosen 4");
+                }
+                else if (playerResponse == "exit")
+                {
+                    Console.WriteLine("Exiting store");
+                    Console.ReadLine();
+                    doneViewingStoreItems = true;
+                }
+                else
+                {
+                    Console.WriteLine("That's not an item in the store. Please choose again");
+                }
+            }
+
+            //  Use "IF" statement to find the selection the user made
+
+            
+
+            Console.ReadLine();
+
 
         }
     }
