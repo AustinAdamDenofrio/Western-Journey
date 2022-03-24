b32d9a80a21acd9b8a7a7fb6fa5c6bca7ea23c6d
AustinDenofrio <denofrioaustin@gmail.com>
2022-03-24 15:20:37 -0400

    Added Property for health, name, and hunger for the Playable character class. Added the referance to the name property from the program file to reflect the new properties.

---
+++ OregonTrail/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace OregonTrail
+{
+    /// <summary>
+    /// This is class is the playable main characterc of the game.
+    /// </summary>
+    public class Player
+    {
+        public int Health { get; set; }
+
+        public int Hunger { get; set; }
+    }
+}
